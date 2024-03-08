9ca0b9bd3bd1390d459eb13f7f14e31037d05db4
Naveen-gn <naveen2003gn@gmail.com>
2024-03-08 22:32:50 +0530

    Sign page completed

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -18,8 +18,8 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <div className="flex-shrink-0 text-white">
-              Logo
+            <div className="flex-shrink-0 text-white bg-gradient-to-r from-blue-700 via-grey-300 to-grey-500 p-1 rounded-full px-2">
+              N-chat
             </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 export default function Signin() {
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(passwordShown ? false : true);
+  };
   return (
 <div className='w-full h-screen bg-slate-900 flex flex-wrap justify-center '>
 
 
-<div className='w-full sm:w-1/2 md:w-full lg:w-1/2  '>
-  <h1>div1</h1>
+<div className='w-full sm:w-1/2 md:w-full lg:w-1/2  flex justify-center items-center '>
+<div className="flex flex-col justify-center items-center gap-4">
+  <Link to='/' className='text-decoration-none text-white bg-gradient-to-r from-blue-700 via-grey-300 to-grey-500  rounded-full text-3xl p-4'>N-chat</Link>
+  <div className='w-3/4 sm:3/4'>
+  <h1 className='text-white text-center'>You're connecting with friends, family, or colleagues, our platform provides a seamless and secure way to stay connected.</h1>
+  </div>
+  
+  </div>
   </div>
 
 <div className="  w-full sm:w-1/2 md:w-full lg:w-1/2  flex justify-center items-center " >
-  <div  className='rounded-lg p-12 border border-blue-600 w-full   md:w-3/5 shadow shadow-slate-700'>
+  <div  className='p-12 rounded-3xl border border-blue-600 w-full   md:w-3/5 shadow shadow-slate-700'>
   <h4 className='text-white mb-6 text-center '>Sign in</h4>
         <div className='flex flex-col gap-6 justify-center  '>
         <input
@@ -19,11 +31,28 @@ export default function Signin() {
           className=" w-full px-4 py-2 rounded-2xl border border-blue-600 focus:outline-none"
           placeholder="example@gmail.com"
         />
-        <input
+        <div className="relative">
+      <input
+        type={passwordShown ? "text" : "password"}
+        className="w-full px-4 py-2 border border-blue-600 rounded-2xl focus:outline-none"
+        placeholder="password"
+        autoComplete='off'
+        onCopy={(e) => e.preventDefault()}
+      />
+      <i 
+        onClick={togglePasswordVisiblity} 
+        className="absolute top-2 right-2 cursor-pointer text-blue-800"
+      >
+        {passwordShown ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />}
+      </i>
+    </div>
+      
+        {/* <input
           type="password"
           className=" w-full px-4 py-2 border border-blue-600 rounded-2xl focus:outline-none"
           placeholder="password"
-        />
+          
+        /> */}
         <button  className="px-4 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-500 text-center ">Signin</button>
         </div>
         <div className='flex flex-col gap-4 mt-5'>
