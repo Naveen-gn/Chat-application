17c098ba1980aac1abfdd68d27f7efaf309ee807
Naveen-gn <naveen2003gn@gmail.com>
2024-03-09 12:23:12 +0530

    About Page Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,17 @@ import { BrowserRouter as Router, Routes,Route  } from 'react-router-dom'
 import Navbar from './Components/Navbar'
 import Signin from './Pages/Signin'
 import Signup from './Pages/Signup'
+import About from './Pages/About'
 
 export default function App() {
+  
   return (
    <Router>
     <Navbar/>
     <Routes>
       <Route path='/signup' element={<Signup/>}/>
       <Route path='/signin' element={<Signin/>}/>
+      <Route path='/about' element={<About/>}/>
     </Routes>
    </Router>
   )
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -31,9 +31,9 @@ const Navbar = () => {
 
             <div className="hidden md:block ">
               <div className="ml-10 flex  items-baseline space-x-4">
-                <Link to="/" onClick={closeMenu} className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</Link>
-                <Link to="/about" onClick={closeMenu} className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</Link>
-                <Link to="/signin" onClick={closeMenu} className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ">Sign in</Link>
+                <Link to="/" onClick={closeMenu} className="text-gray-300  hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium">Home</Link>
+                <Link to="/about" onClick={closeMenu} className="text-gray-300  hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium">About</Link>
+                <Link to="/signin" onClick={closeMenu} className="text-gray-300  hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium ">Sign in</Link>
               </div>
             </div>
 
