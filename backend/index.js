b78cde1ac5c27ee5bef06f3e180a279118951dea
Naveen-gn <naveen2003gn@gmail.com>
2024-03-10 00:07:58 +0530

    Backend added

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => console.log('MongoDB is connected')).catch(err => console.error(err));
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄" });
+});
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message: err.message || 'Internal Server Error',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000',
+      secure: false,
+    },
+  },
   plugins: [react()],
+  build: {
+    outDir: './dist',
+    emptyOutDir: false,// Specify the output directory
+  }
 })
