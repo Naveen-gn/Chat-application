1132551d3869fe3e888a2c53df321cb7400404a2
Naveen-gn <naveen2003gn@gmail.com>
2024-04-12 19:23:10 +0530

    logout route & controller added successfully

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -102,6 +102,19 @@ export const signin = async (req, res, next) => {
 };
 
 
+export const logout= async (req,res)=>{
+  try {
+    res.cookie("jwt","",{maxAge:0})
+    res.status(200).json({
+      message:"Logout Sucessfully"
+    })
+  } catch (error) {
+    console.log("Internal server error", error.message);
+    res.status(500).json({
+      error:"Internal server error"
+    })
+  }
+}
 
 
 
