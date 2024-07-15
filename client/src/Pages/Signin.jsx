a7afb062f57bd93075c2eeeef61803026d817997
Naveen-gn <naveen2003gn@gmail.com>
2024-07-15 15:28:42 +0530

    Login , signin new ui completed

--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -1,10 +1,8 @@
-import React, { useState } from 'react'
-import { Link,useNavigate } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-import Chaticon from '../assets/chat.png'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Chaticon from "../assets/chat.png";
 
-const baseUrl = 'http://localhost:5000/';
+const baseUrl = "http://localhost:5000/";
 
 export default function Signin() {
   const [passwordShown, setPasswordShown] = useState(false);
@@ -12,87 +10,104 @@ export default function Signin() {
     setPasswordShown(passwordShown ? false : true);
   };
   const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
   const [formData, setFormData] = useState({});
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });     
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
- 
+
   return (
-<div className='Signin w-full h-screen flex flex-wrap justify-center overflow-y-auto '>
-
-
-<div className='w-full mb-10  md:w-full lg:w-1/2 xl:w-1/2 2xl:w-1/2  flex justify-center items-center flex-col '>
-
-<div className="">
-  <img src={Chaticon} className='w-80 h-72' />
-  {/* <Link to='/' className='text-decoration-none text-white bg-gradient-to-r from-blue-700 via-grey-300 to-grey-500  rounded-full text-3xl p-4'>N-chat</Link> */}
-  </div>
-  
-  <div className='w-3/4 sm:3/4'>
-  <h1 className='text-white text-center'>You're connecting with friends, family, or colleagues, our platform provides a seamless and secure way to stay connected.</h1>
-  </div>
-
-
-  </div>
-
-<div className="w-full  md:w-full lg:w-1/2 xl:w-1/2 2xl:w-1/2 flex justify-center items-center  " >
-  <div  className='w-96 p-12 rounded-3xl border border-blue-600    shadow shadow-slate-70 mb-20'>
-  <h4 className='text-white mb-6 text-center '>Login</h4>
-        <div className='flex flex-col gap-6 justify-center '>
-          <form className='flex flex-col gap-6 justify-center  ' >
-        <input
-          type="text"
-          className=" w-full px-4 py-2 rounded-2xl border border-blue-600 focus:outline-none"
-          placeholder="Username"
-          id='username'
-          onChange={handleChange}
-        />
-        <div className="relative">
-      <input
-        type={passwordShown ? "text" : "password"}
-        className="w-full px-4 py-2 border border-blue-600 rounded-2xl focus:outline-none"
-        placeholder="password"
-        autoComplete='off'
-        onCopy={(e) => e.preventDefault()}
-        id='password'
-        onChange={handleChange}
-      />
-      <i 
-        onClick={togglePasswordVisiblity} 
-        className="absolute top-2 right-2 cursor-pointer text-blue-800"
-      >
-        {passwordShown ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />}
-      </i>
-    </div>
-        <button className="px-4 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-500 text-center " type='submit' disabled={loading}>
-            {
-              loading ? (<>
-                <span className='ml-2'>Signing in...</span>
-              </>
-              ) : 'Sign in'
-            }
-            </button>
-        </form>
+    <div className="Signin w-full h-screen flex flex-wrap justify-center overflow-y-auto ">
+      <div className="w-full mb-10  md:w-full lg:w-1/2 xl:w-1/2 2xl:w-1/2  flex justify-center items-center flex-col ">
+        <div className="">
+          <img src={Chaticon} className="w-80 h-72" />
         </div>
-        
-        {
-            errorMessage && <h3 className='mt-5 text-red-600'>
-              {errorMessage}
-            </h3>
-          }
+
+        <div className="w-3/4 sm:3/4">
+          <h1 className="text-white text-center">
+            You're connecting with friends, family, or colleagues, our platform
+            provides a seamless and secure way to stay connected.
+          </h1>
+        </div>
       </div>
 
-  </div>
-        
+      <div className="w-full  md:w-full lg:w-1/2 xl:w-1/2 2xl:w-1/2 flex justify-center items-center  ">
+        <div className="w-96 p-12 pt-7 rounded-3xl border border-gray-600 bg-[#0b111f]    shadow-2xl shadow-slate-70 mb-20">
+          <h4 className="text-white mb-6 text-center text-lg font-normal">
+            Login
+          </h4>
+          <div className="flex flex-col gap-6 justify-center ">
+            <form className="flex flex-col gap-6 justify-center  ">
+              <label className="input input-bordered flex items-center gap-2 rounded-2xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                </svg>
+                <input
+                  type="text"
+                  className="grow rounded-2xl"
+                  placeholder="Username"
+                  id="username"
+                  onChange={handleChange}
+                />
+              </label>
+              <label className="input input-bordered flex items-center gap-2 rounded-2xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <input
+                  type="password"
+                  className="grow rounded-2xl"
+                  placeholder="password"
+                  id="password"
+                  onChange={handleChange}
+                />
+              </label>
 
+              <button
+                className="px-4 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-500 text-center "
+                type="submit"
+                disabled={loading}
+              >
+                {loading ? (
+                  <>
+                    <span className="ml-2">Signing in...</span>
+                  </>
+                ) : (
+                  "Login"
+                )}
+              </button>
+            </form>
+          </div>
+          <h4 className="text-white mt-5 text-md font-normal">
+            Don't haven an account !{" "}
+            <Link
+              to="/signup"
+              className="text-decoration-none text-blue-600 hover:text-blue-500 text-md font-medium"
+            >
+              Signup
+            </Link>
+          </h4>
 
-      
-   
-
-</div>
-
-   
-  )
+          {errorMessage && (
+            <h3 className="mt-5 text-red-600">{errorMessage}</h3>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -1,19 +1,12 @@
-import React, { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-import Chaticon from '../assets/chat.png'
-import { Link,useNavigate } from 'react-router-dom'
-import { AiOutlineDown } from 'react-icons/ai';
-const baseUrl = 'http://localhost:5000/';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import Chaticon from "../assets/chat.png";
+import { Link, useNavigate } from "react-router-dom";
+import { AiOutlineDown } from "react-icons/ai";
+const baseUrl = "http://localhost:5000/";
 
 export default function Signin() {
-  const [isOpen, setIsOpen] = useState(false);
-    const [selectedOption, setSelectedOption] = useState('Select Gender');
-    const options=["male","female"]
-    const handleOptionClick = (option) => {
-      setSelectedOption(option);
-      setIsOpen(false);
-    };
   const [passwordShown, setPasswordShown] = useState(false);
   const togglePasswordVisiblity = () => {
     setPasswordShown(passwordShown ? false : true);
@@ -21,150 +14,178 @@ export default function Signin() {
   const [formData, setFormData] = useState({});
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });     
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
 
-  const handleSubmit=async(e)=>{
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     if (!formData.username || !formData.email || !formData.password) {
-      return setErrorMessage('Please fill all the fields');
+      return setErrorMessage("Please fill all the fields");
     }
     try {
       setLoading(true);
       setErrorMessage(null);
-      const res=await fetch(`${baseUrl}api/auth/signup`,{
-        method:'POST',
-        headers:{
-          'Content-Type':'application/json'
+      const res = await fetch(`${baseUrl}api/auth/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        body:JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
-      const data=await res.json()
+      const data = await res.json();
       console.log(data);
-      if (data.success===false) {
+      if (data.success === false) {
         setLoading(false);
-       return setErrorMessage("Username or Email already exists! Please try again.");
-       
+        return setErrorMessage(
+          "Username or Email already exists! Please try again."
+        );
       }
       if (res.ok) {
-        navigate('/signin')
+        navigate("/signin");
       }
-    } 
-    catch (error) {
-      setErrorMessage(error.message || "Something went wrong! Please try again.");
+    } catch (error) {
+      setErrorMessage(
+        error.message || "Something went wrong! Please try again."
+      );
       setLoading(false);
     }
   };
 
   return (
-<div className='Signup w-full h-screen flex flex-wrap justify-center overflow-y-auto'>
-
-
-<div className='w-full mb-10  md:w-full lg:w-1/2 xl:w-1/2 2xl:w-1/2  flex justify-center items-center flex-col '>
-
-<div className="">
-  <img src={Chaticon} className='w-80 h-72' />
-  {/* <Link to='/' className='text-decoration-none text-white bg-gradient-to-r from-blue-700 via-grey-300 to-grey-500  rounded-full text-3xl p-4'>N-chat</Link> */}
-  </div>
-  
-  <div className='w-3/4 sm:3/4'>
-  <h1 className='text-white text-center'>You're connecting with friends, family, or colleagues, our platform provides a seamless and secure way to stay connected.</h1>
-  </div>
-
-
-  </div>
-
-<div className="w-full  md:w-full lg:mt-10 lg:w-1/2 xl:w-1/2 2xl:w-1/2 flex justify-center items-center  " >
-  <div  className='w-96 p-12 rounded-3xl border border-blue-600  mb-20  shadow shadow-slate-70'>
-  <h4 className='text-white mb-6 text-center '>Sign up</h4>
-        <div className='flex flex-col gap-6 justify-center  '>
-          
-          <form className='flex flex-col gap-6 justify-center' onSubmit={handleSubmit} >
-        <input
-          type="text"
-          className=" w-full px-4 py-2 rounded-2xl border border-blue-600 focus:outline-none"
-          placeholder="fullname"
-          id='fullname'
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          className=" w-full px-4 py-2 rounded-2xl border border-blue-600 focus:outline-none"
-          placeholder="username"
-          id='username'
-          onChange={handleChange}
-        />
-        
-
-
-        <div className="relative">
-      <input
-        type={passwordShown ? "text" : "password"}
-        className="w-full px-4 py-2 border border-blue-600 rounded-2xl focus:outline-none"
-        placeholder="password"
-        autoComplete='off'
-        id='password'
-        onChange={handleChange}
-        onCopy={(e) => e.preventDefault()}
-      />
-      <i 
-        onClick={togglePasswordVisiblity} 
-        className="absolute top-2 right-2 cursor-pointer text-blue-800"
-      >
-        {passwordShown ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />}
-      </i>
-    </div>
-
-
-       
-    
-    <div className='relative'>
-    <button onClick={() => setIsOpen(!isOpen)} className='bg-white px-4 py-2 border border-blue-600  focus:outline-none flex justify-between items-center p-3 gap-3 rounded-xl w-full text-gray-400'>
-          {selectedOption}
-          <AiOutlineDown className='ml-2 text-blue-600 font-bold' />
-        </button>
-        {isOpen && (
-          <ul className="options absolute  top-8 bg-white border-2 border-slate-200 w-full mt-2 py-1 rounded-xl shadow-lg overflow-y-auto">
-            {options.map((options) => (
-              <li key={options} className="py-1 px-4 hover:bg-gray-100 cursor-pointer" onClick={() => handleOptionClick(options)}>
-                {options}
-              </li>
-            ))}
-          </ul>
-        )}
+    <div className="Signup w-full h-screen flex flex-wrap justify-center overflow-y-auto">
+      <div className="w-full mb-10  md:w-full lg:w-1/2 xl:w-1/2 2xl:w-1/2  flex justify-center items-center flex-col ">
+        <div className="">
+          <img src={Chaticon} className="w-80 h-72" />
         </div>
-    <button className="px-4 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-500 text-center " type='submit' disabled={loading}>
-            {
-              loading ? (<>
-                <span className='ml-2'>Signing up...</span>
-              </>
-              ) : 'Sign Up'
-            }
-            </button>
-        </form>
-        
+
+        <div className="w-3/4 sm:3/4">
+          <h1 className="text-white text-center text-md font-normal">
+            You're connecting with friends, family, or colleagues, our platform
+            provides a seamless and secure way to stay connected.
+          </h1>
         </div>
-        
-       
-        <h4 className='text-white mt-5'>Already haven an account! <Link to='/signin' className='text-decoration-none text-blue-600 hover:text-blue-500 '>Sign in</Link></h4>
-        {
-            errorMessage && <h3 className='mt-5 text-red-600'>
-              {errorMessage}
-            </h3>
-          }
       </div>
 
-  </div>
-        
+      <div className="w-full  md:w-full lg:mt-10 lg:w-1/2 xl:w-1/2 2xl:w-1/2 flex justify-center items-center  ">
+        <div className="w-96 p-12 pt-7 rounded-3xl border border-gray-600 bg-[#0b111f]  mb-20  shadow-2xl shadow-slate-70">
+          <h4 className="text-white mb-6 text-center text-lg font-normal">Sign up</h4>
+          <div className="flex flex-col gap-6 justify-center  ">
+            <form
+              className="flex flex-col gap-6 justify-center"
+              onSubmit={handleSubmit}
+            >
+              <input
+                type="text"
+                placeholder="Fullname"
+                className="input input-bordered w-full max-w-xs rounded-2xl text-white"
+                id="fullname"
+                onChange={handleChange}
+              />
+              <label className="input input-bordered flex items-center gap-2 rounded-2xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                </svg>
+                <input
+                  type="text"
+                  className="grow rounded-2xl"
+                  placeholder="Username"
+                  id="username"
+                  onChange={handleChange}
+                />
+              </label>
+              <label className="input input-bordered flex items-center gap-2 rounded-2xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <input
+                  type="password"
+                  className="grow rounded-2xl"
+                  placeholder="password"
+                  id="password"
+                  onChange={handleChange}
+                />
+              </label>
 
+              <h1 className="text-white font-normal">Gender</h1>
+              <div className="flex  gap-4">
+                <div className="flex items-center gap-1">
+                  <input
+                    type="radio"
+                    name="gender"
+                    id="male"
+                    value="male"
+                    className="radio"
+                  />
+                  <label
+                    htmlFor="male"
+                    className="text-sm font-light text-white"
+                  >
+                    Male
+                  </label>
+                </div>
+                <div className="flex items-center gap-1">
+                  <input
+                    type="radio"
+                    name="gender"
+                    value="female"
+                    id="female"
+                    className="radio"
+                  />
+                  <label
+                    htmlFor="female"
+                    className="text-sm font-light text-white"
+                  >
+                    Female
+                  </label>
+                </div>
+              </div>
 
-      
-   
+              <button
+                className="px-4 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-500 text-center "
+                type="submit"
+                disabled={loading}
+              >
+                {loading ? (
+                  <>
+                    <span className="ml-2">Signing up...</span>
+                  </>
+                ) : (
+                  "Sign Up"
+                )}
+              </button>
+            </form>
+          </div>
 
-</div>
-
-   
-  )
+          <h4 className="text-white mt-5 text-md font-normal">
+            Already haven an account !{" "}
+            <Link
+              to="/signin"
+              className="text-decoration-none text-blue-600 hover:text-blue-500 text-md font-medium"
+            >
+              Login
+            </Link>
+          </h4>
+          {errorMessage && (
+            <h3 className="mt-5 text-red-600">{errorMessage}</h3>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
