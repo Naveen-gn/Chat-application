d97d13e35e8b0992be22c1a64c16248290ca0f4a
Naveen-gn <naveen2003gn@gmail.com>
2024-07-15 12:04:27 +0530

    Get user route, controller completed

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
-import cookieParser from 'cookie-parser';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
+app.use("/api/users",userRoutes)
 app.get('/', (req, res) => {
     res.send('Server is working.');
     }
