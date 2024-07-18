87e09a6a53e375081a5368de6e3a7d0bb21e1752
Naveen-gn <naveen2003gn@gmail.com>
2024-07-18 17:10:50 +0530

    backend index js file change

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -24,10 +25,22 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
 app.use("/api/users",userRoutes)
-app.get('/', (req, res) => {
-    res.send('Server is working.');
-    }
-);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get("/", (req, res) => {
+  res.json({ message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄" });
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message: err.message || 'Internal Server Error',
+  });
+});
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
