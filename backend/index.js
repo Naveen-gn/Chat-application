fbc26b57e47f7f605e225e6b9295b9c40d9d1042
Naveen-gn <naveen2003gn@gmail.com>
2024-07-19 15:41:32 +0530

    cors options / removed

--- backend/index.js
+++ backend/index.js
@@ -17,27 +17,27 @@ mongoose
   .then(() => console.log('MongoDB is connected'))
   .catch(err => console.error(err));
 
-  // const allowedOrigins = [
-  //   'https://naveen-chat-app.vercel.app/',
-  //   'http://localhost:3000'
-  // ];
+  const allowedOrigins = [
+    'https://naveen-chat-app.vercel.app',
+    'http://localhost:3000'
+  ];
   
-  // const corsOptions = {
-  //   origin: function (origin, callback) {
-  //     if (!origin) return callback(null, true);
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
   
-  //     if (allowedOrigins.indexOf(origin) !== -1) {
-  //       callback(null, true);
-  //     } else {
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  //   methods: ['GET', 'POST'],
-  //   allowedHeaders: ['Content-Type', 'Authorization'],
-  // };
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
   
   
-  app.use(cors());
+  app.use(cors(corsOptions));
   app.use(express.json());
   app.use(cookieParser());
   
