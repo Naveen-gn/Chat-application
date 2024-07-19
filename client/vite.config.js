a334f22d08dcac27acc43f1ad0541d21a20ec7ac
Naveen-gn <naveen2003gn@gmail.com>
2024-07-19 15:20:57 +0530

    backend link change in vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'https://chat-app-server-chi-three.vercel.app',
       secure: false,
     },
   },
