f7ef18065e1f17875cef5ca972b34ddb13ecfcac
Naveen-gn <naveen2003gn@gmail.com>
2024-07-19 14:26:38 +0530

    cors removed

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -5,12 +5,7 @@ import express from 'express';
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server,{
-    cors: {
-        origin: "https://chat-app-cyan-xi.vercel.app/",
-        methods: ["GET", "POST"]
-    }
-});
+const io = new Server(server);
 
 export const getReceiverSocketId = (receiverId) => {
     return userSocketMap[receiverId];
