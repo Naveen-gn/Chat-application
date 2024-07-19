5dab4ed63fc2186907d6e6978918e9df7e1487b2
Naveen-gn <naveen2003gn@gmail.com>
2024-07-19 14:39:16 +0530

    backend struct modification

--- backend/index.js
+++ backend/index.js
@@ -12,24 +12,29 @@ import { app, server } from './socket/socket.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log('MongoDB is connected'))
   .catch(err => console.error(err));
+  
+  app.use(cors());
+  app.use(express.json());
+  app.use(cookieParser());
+  
+  const PORT = process.env.PORT || 5000;
+  
+  app.get("/", (req, res) => {
+    res.json({ message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄" });
+  });
+  
+  app.use("/api/auth",authRoutes)
+  app.use("/api/message",messageRoutes)
+  app.use("/api/users",userRoutes)
 
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
-app.use("/api/auth",authRoutes)
-app.use("/api/message",messageRoutes)
-app.use("/api/users",userRoutes)
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 
-app.get("/", (req, res) => {
-  res.json({ message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄" });
-});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
