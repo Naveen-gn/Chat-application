5bacc859f6034b859e98394d1b1975b6017fcac5
Naveen-gn <naveen2003gn@gmail.com>
2024-07-22 13:41:37 +0530

    socket cors option change

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,11 +1,17 @@
 import {Server} from 'socket.io';
 import http from 'http';
 import express from 'express';
+import cors from 'cors';
 
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server,
+    {
+        cors: {
+            origin: '*',
+        }
+    });
 
 export const getReceiverSocketId = (receiverId) => {
     return userSocketMap[receiverId];
