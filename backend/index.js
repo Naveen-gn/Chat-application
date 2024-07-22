45f72f63b64b5ac01b0485da593916b1aec975ad
Naveen-gn <naveen2003gn@gmail.com>
2024-07-22 13:36:17 +0530

    cors option removed

--- backend/index.js
+++ backend/index.js
@@ -18,20 +18,9 @@ mongoose
   .connect(process.env.MONGO)
   .then(() => console.log('MongoDB is connected'))
   .catch(err => console.error(err));
-
-var whitelist = ['https://naveen-chat-app.vercel.app', 'https://vercel.com' ,'http://localhost:3000']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
   
 
-  app.use(cors(corsOptions));
+  app.use(cors());
   app.use(express.json());
   app.use(cookieParser());
   
