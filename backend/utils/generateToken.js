6d044dddc5ebc1b61bb55cf5bfd8f6ebc3067aea
Naveen-gn <naveen2003gn@gmail.com>
2024-07-23 19:29:24 +0530

    generate token httpOnly changed

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,7 @@ const generateTokenAndCookie =(userId,res)=>{
     });
     const cookieOptions = {
         maxAge:15*24*60*60*1000,
-        httpOnly:true,//cookie can't be accessed by client side script
+        httpOnly:false,//cookie can't be accessed by client side script
         sameSite:"strict",//cookie will only be sent in a first-party context
         secure:process.env.NODE_ENV === "production" ? true : false,//cookie will only be sent in https
     };
