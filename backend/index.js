8ce5d7a58e6846f0ac18b509904deb8050f5109a
Naveen-gn <naveen2003gn@gmail.com>
2024-08-24 14:53:42 +0530

    cors removed for index.js

--- backend/index.js
+++ backend/index.js
@@ -17,18 +17,18 @@ mongoose
   .then(() => console.log('MongoDB is connected'))
   .catch(err => console.error(err));
   
-var whitelist = ['http://localhost:3000','https://nchatapp.vercel.app']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// var whitelist = ['http://localhost:3000','https://nchatapp.vercel.app']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
-  app.use(cors(corsOptions));
+  app.use(cors());
   app.use(express.json());
   app.use(cookieParser());
   
