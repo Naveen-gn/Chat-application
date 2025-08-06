6d5476eb5a4b3be6dfc17d96ee5053e5471c78b2
Naveen-gn <naveen2003gn@gmail.com>
2025-08-07 00:02:52 +0530

    frontend to client change

--- backend/index.js
+++ backend/index.js
@@ -45,10 +45,10 @@ var corsOptions = {
   app.use("/api/message",messageRoutes)
   app.use("/api/users",userRoutes)
   
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 
 
