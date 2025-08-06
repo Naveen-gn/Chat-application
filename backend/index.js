fdbd7bb0004bbada151bcde66ec35d3a50a0484d
Naveen-gn <naveen2003gn@gmail.com>
2025-08-07 00:40:04 +0530

    unwanted get requests removed in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,67 +1,36 @@
-import express from 'express';
-import path from 'path';
-// import cors from 'cors';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-import userRoutes from './routes/user.route.js';
-import { app, server } from './socket/socket.js';
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO)
-  .then(() => console.log('MongoDB is connected'))
-  .catch(err => console.error(err));
-  
-// var whitelist = ['http://localhost:3000','https://nchatapp.vercel.app']
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
+  .then(() => console.log("MongoDB is connected"))
+  .catch((err) => console.error(err));
 
-//   app.use(cors(corsOptions));
-  app.use(express.json());
-  app.use(cookieParser());
-  const PORT = process.env.PORT || 5000;
-  const __dirname = path.resolve();
+app.use(express.json());
+app.use(cookieParser());
 
-  
-  
-  app.get('/', (req, res) => {
-    res.json({ message: 'API is working'});
-  });
-  
-  app.use("/api/auth",authRoutes)
-  app.use("/api/message",messageRoutes)
-  app.use("/api/users",userRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/users", userRoutes);
 
-  app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
- app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
-
-
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message: err.message || 'Internal Server Error',
-  });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    }
-);
+  console.log(`Server is running on port ${PORT}`);
+});
